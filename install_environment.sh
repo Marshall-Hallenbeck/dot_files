b92fb809b9efb11b23793ef77dc78a039849ecef
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2022-07-11 11:47:11 -0400

    Update install_environment.sh

--- install_environment.sh
+++ install_environment.sh
@@ -9,5 +9,6 @@ wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.tm
 tmux source ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.zshrc" -O ~/.zshrc
+echo "Installing virtualenvwrapper via pip3"
 pip3 install virtualenvwrapper
 echo "Environment should now be set up!"
