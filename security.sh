9f8d27261faf93817f6b78f0162996881a6b2da4
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2023-04-12 11:10:52 -0400

    Update security.sh

--- security.sh
+++ security.sh
@@ -24,6 +24,22 @@ git clone https://github.com/fortra/impacket.git ~/pentest/tools/ad_and_windows/
 
 ### TOOLS
 echo "Downloading and installing tools"
+echo "Making sure nmap, netcat, and socat are installed"
+sudo apt install libpcap-dev nmap socat netcat-traditional
+
+echo "Installing odat (Oracle DB testing tool)"
+sudo apt install odat
+
+echo "Installing Kerberos User Package"
+sudo apt install krb5-user
+
+echo "Installing Golang"
+sudo apt install golang
+
+echo "Adding Golang path to .zshrc"
+echo 'export GOROOT=/usr/local/go' >> ~/.zshrc
+echo 'export GOPATH=$HOME/go' >> ~/.zshrc
+echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH' >> ~/.zshrc 
 
 echo "Installing pipx"
 python3 -m pip install pipx
@@ -35,8 +51,18 @@ pipx install crackmapexec
 echo "Installing Sliver via install script"
 curl https://sliver.sh/install|sudo bash
 
-echo "Making sure nmap, netcat, and socat are installed"
-sudo apt install nmap socat netcat-traditional
+echo "Installing Project Discovery tools"
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install -v github.com/projectdiscovery/proxify/cmd/proxify@latest
+go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 
-echo "Installing Kerberos User Package"
-sudo apt install krb5-user
+echo "Making sure nuclei engine & templates are up to date"
+nuclei -un
+nuclei -ut
+
+echo "Installing ffuf"
+go install github.com/ffuf/ffuf/v2@latest
