79bdcdd4d92d2b24a499170fcdc648523b4b6331
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2025-03-01 13:13:37 -0500

    change installation method of virtualenvwrapper

--- install_environment.sh
+++ install_environment.sh
@@ -4,7 +4,7 @@
 echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/1000-force-ipv4-transport
 
 sudo apt update
-sudo apt install -y zsh tmux vim python3-pip git
+sudo apt install -y zsh tmux vim python3-pip git virtualenvwrapper
 wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.bash_aliases" -O ~/.bash_aliases
 wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.vimrc" -O ~/.vimrc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -12,6 +12,4 @@ wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.tm
 tmux source ~/.tmux.conf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 wget "https://raw.githubusercontent.com/Marshall-Hallenbeck/dot_files/master/.zshrc" -O ~/.zshrc
-echo "Installing virtualenvwrapper via pip3"
-pip3 install virtualenvwrapper
 echo "Environment should now be set up!"
